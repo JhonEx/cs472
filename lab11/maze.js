01bcd01a4f011d3f544dc0a60112b8d800dc7145
Jhon Ramirez <sir.edwards@gmail.com>
2020-08-05 23:32:56 -0500

    lab11

---
+++ lab11/maze.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    let $boundary = $(".boundary");
+    let $status = $("#status");
+
+    $boundary.mousemove(function() {   
+        lost();       
+    });
+    function lost(){
+        if (!$boundary.hasClass("youlose")){
+            $boundary.addClass("youlose");      
+            $status.text("Game Over, refresh page to try again");
+            $status.addClass('text')
+        }
+    }
+});
+
